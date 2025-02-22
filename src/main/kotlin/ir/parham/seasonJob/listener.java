522c52f9e25ed4b93381c0703d2a51efa7157620
Parham099 <parhamahmady522@gmail.com>
2025-02-22 12:06:51 +0330

    2.0.0

---
+++ src/main/kotlin/ir/parham/seasonJob/listener.java
@@ -0,0 +1,12 @@
+package ir.parham.seasonJob;
+
+
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.player.PlayerJoinEvent;
+
+public class listener {
+    @EventHandler
+    public void onJoin(PlayerJoinEvent event) {
+
+    }
+}
